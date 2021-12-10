ec8ac97b660e82006806db46e00e83832f78d59d
wesinalves <wesinec@gmail.com>
2021-12-10 07:12:18 -0300

    check token on preload

--- src/screens/Preload/index.tsx
+++ src/screens/Preload/index.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, LoadingIcon } from './styles';
 import { Image, ImageBackground } from 'react-native';
 import PcdtLogo from '../../assets/pcdt.png';
 import Background from '../../assets/background.png';
 
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation } from '@react-navigation/native';
+
 
 
 export default () => {
+    const navigation = useNavigation();
+    useEffect(()=>{
+        const checkToken = async () => {
+            const token = await AsyncStorage.getItem('token');
+
+            if(token !== null){
+                // validate token
+            }else{
+                navigation.navigate("Login" as never);
+            }
+        }
+    }, []);
     return (
         <Container>
             <ImageBackground source={Background} resizeMode="cover" style={{flex:1, justifyContent:"center", alignItems: "center"}}> 
