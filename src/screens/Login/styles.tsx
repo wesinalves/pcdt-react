5f0931803cb67858c868ef67d4ffbbec864ec5af
wesinalves <wesinec@gmail.com>
2021-12-10 14:44:09 -0300

    create form components

--- src/screens/Login/styles.tsx
+++ src/screens/Login/styles.tsx
@@ -6,3 +6,20 @@ export const Container = styled.SafeAreaView`
    justify-content: center;  
 
 `;
+
+export const InputArea = styled.View`
+    padding: 40px;
+    width: 100%; 
+        
+`;
+export const CustomButton = styled.TouchableOpacity`
+    height: 60px;
+    background-color: #268596;
+    border-radius: 10px;     
+    justify-content: center;
+    align-items: center;    
+`;
+export const CustomButtonText = styled.Text`
+    font-size: 18px;
+    color: #FFF;    
+`;
