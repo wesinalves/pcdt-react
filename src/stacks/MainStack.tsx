0e686614a5f4f3d28e39f03a04a120395996cb42
wesinalves <wesinec@gmail.com>
2021-12-09 22:01:14 -0300

    add initial route

--- src/stacks/MainStack.tsx
+++ src/stacks/MainStack.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack'
 import Login from '../screens/Login';
+import Preload from '../screens/Preload';
 
 const Stack = createStackNavigator();
 
 const MainStack = () => {
     return (
-        <Stack.Navigator>
+        <Stack.Navigator 
+            initialRouteName="Preload"
+            screenOptions={{
+                headerShown: false
+            }}
+        > 
+            <Stack.Screen name='Preload' component={Preload} />
             <Stack.Screen name='Login' component={Login} />
         </Stack.Navigator>
     );
