52ab660ca1cbfc40307d6bba80a2d8ef2a30fe27
wesinalves <wesinec@gmail.com>
2021-12-09 22:01:36 -0300

    remove background and add loading icon

---
+++ src/screens/Preload/styles.tsx
@@ -0,0 +1,24 @@
+import { useLinkProps } from '@react-navigation/native';
+import React from 'react';
+import { ImageBackgroundProps } from 'react-native';
+import styled from 'styled-components/native';
+
+export const Container = styled.SafeAreaView`
+   flex: 1;
+   justify-content: center;  
+
+`;
+
+// display: flex;
+// flex-direction: column;
+// justify-content: center;
+// align-items: center    
+// height: 100%;
+// background-image: url('../../assets/background.png');
+// background-position: center top;
+// background-repeat: no-repeat;
+// background-size: cover;
+
+export const LoadingIcon = styled.ActivityIndicator`
+    margint-top: 50px
+`;
