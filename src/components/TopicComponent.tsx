e0cad46ada3b2a07622b103db35d54c1e7916329
wesinalves <wesinec@gmail.com>
2021-12-30 16:05:16 -0300

    update topic component

--- src/components/TopicComponent.tsx
+++ src/components/TopicComponent.tsx
@@ -1,33 +1,45 @@
 import React from "react";
 import styled from "styled-components/native";
 import Trophy from '../assets/trophy.svg';
-import { useNavigation} from '@react-navigation/native'; 
+import { useNavigation } from '@react-navigation/native'; 
 
 const TopicArea = styled.View`
-    margin: 0px 20px 5px 20px;
+    margin: 0px 10px 5px 10px;
     border-radius: 10px;
     flex-direction: column;
-    height: 50px;
-     
+    height: 100px; 
+    background-color: #fff;
     
 `
 const TopicItem = styled.TouchableOpacity`
     flex: 1;
-    justify-content: center;
     align-items: center;
-    
-    
+    flex-direction: row;
+    justify-content: space-between;
+        
+`;
+
+const InfoView = styled.View`
+    flex: 1;
+    align-items: center;
+    flex-direction: row;
+    justify-content: flex-end;
 `;
 
 const TopicPercent = styled.Text`
     margin-right: 10px;
+    margin-left: 10px;
     color: #000;
-`
+    font-size: 20px;                   
+`;
 
 const TopicLabel = styled.Text`
-  margin-top: 20px;
+  margin-top: 18px;
+  margin-right: 10px;
   margin-bottom: 20px;
+  margin-left: 10px;
   color: #000;
+  font-size: 25px;
 `
 
 interface TopicProps {
@@ -49,11 +61,13 @@ const TopicComponent: React.FC<TopicProps> = ({id, name, percent}) => {
     return (
         <TopicArea>
             <TopicItem onPress={()=>goToDetails(id)}>
-                <TopicLabel>{name}</TopicLabel>
-                {percent === 100 ?
-                    <Trophy height={20} width={20} />
-                : null}
-                <TopicPercent>{percent}</TopicPercent>
+                <TopicLabel>{name.substring(0,20)}</TopicLabel>
+                <InfoView>
+                    {percent === 100 ?
+                        <Trophy height={20} width={20} />
+                    : null}
+                    <TopicPercent>{percent}%</TopicPercent>
+                </InfoView>
             </TopicItem>
         </TopicArea>
         
